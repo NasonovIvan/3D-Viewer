6140462abef34eba9abba085905d7e9068ec3c6b
Kynes Halfhand <kyneshal@mi-o2.msk.21-school.ru>
2023-03-10 00:04:37 +0300

    add photo and gifs

---
+++ src/3dVFront/build-3dV-Desktop_x86_darwin_generic_mach_o_64bit-Debug/CMakeFiles/3dV.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/kyneshal/3D-Viewer/src/Functions/affine.c" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/Functions/affine.c.o" "gcc" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/Functions/affine.c.o.d"
+  "/Users/kyneshal/3D-Viewer/src/Functions/parser.c" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/Functions/parser.c.o" "gcc" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/Functions/parser.c.o.d"
+  "/Users/kyneshal/3D-Viewer/src/giflib/dgif_lib.c" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/dgif_lib.c.o" "gcc" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/dgif_lib.c.o.d"
+  "/Users/kyneshal/3D-Viewer/src/giflib/egif_lib.c" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/egif_lib.c.o" "gcc" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/egif_lib.c.o.d"
+  "/Users/kyneshal/3D-Viewer/src/giflib/gif_err.c" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/gif_err.c.o" "gcc" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/gif_err.c.o.d"
+  "/Users/kyneshal/3D-Viewer/src/giflib/gif_font.c" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/gif_font.c.o" "gcc" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/gif_font.c.o.d"
+  "/Users/kyneshal/3D-Viewer/src/giflib/gif_hash.c" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/gif_hash.c.o" "gcc" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/gif_hash.c.o.d"
+  "/Users/kyneshal/3D-Viewer/src/giflib/gifalloc.c" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/gifalloc.c.o" "gcc" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/gifalloc.c.o.d"
+  "/Users/kyneshal/3D-Viewer/src/giflib/quantize.c" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/quantize.c.o" "gcc" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/giflib/quantize.c.o.d"
+  "/Users/kyneshal/3D-Viewer/src/3dVFront/build-3dV-Desktop_x86_darwin_generic_mach_o_64bit-Debug/3dV_autogen/mocs_compilation.cpp" "CMakeFiles/3dV.dir/3dV_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/3dV.dir/3dV_autogen/mocs_compilation.cpp.o.d"
+  "/Users/kyneshal/3D-Viewer/src/gifimage/qgifimage.cpp" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/gifimage/qgifimage.cpp.o" "gcc" "CMakeFiles/3dV.dir/Users/kyneshal/3D-Viewer/src/gifimage/qgifimage.cpp.o.d"
+  "/Users/kyneshal/3D-Viewer/src/3dVFront/3dV/main.cpp" "CMakeFiles/3dV.dir/main.cpp.o" "gcc" "CMakeFiles/3dV.dir/main.cpp.o.d"
+  "/Users/kyneshal/3D-Viewer/src/3dVFront/3dV/mainwindow.cpp" "CMakeFiles/3dV.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/3dV.dir/mainwindow.cpp.o.d"
+  "/Users/kyneshal/3D-Viewer/src/3dVFront/3dV/myopenglwidget.cpp" "CMakeFiles/3dV.dir/myopenglwidget.cpp.o" "gcc" "CMakeFiles/3dV.dir/myopenglwidget.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
